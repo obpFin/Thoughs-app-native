9390915dabf0f019fa02c53882d6330cca3f6b34
Oskari Peltonen <oskari.peltonen@outlook.com>
2018-05-16 21:06:37 +0300

    Autth screen skeleton

---
+++ src/screens/Auth/Auth.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet
+} from 'react-native';
+
+class AuthScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Login</Text>
+      </View>
+    );
+  }
+}
+
+export default AuthScreen;
