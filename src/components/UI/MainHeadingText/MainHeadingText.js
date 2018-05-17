7e247b5e75d6b991ed0bed2d80a168171c288a81
Oskari Peltonen <oskari.peltonen@outlook.com>
2018-05-17 21:12:25 +0300

    MainHeadingText component

---
+++ src/components/UI/MainHeadingText/MainHeadingText.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+const MainHeadingText = props => (
+  <Text {...props} style={[styles.textHeading, props.style]}>
+    {props.children}
+  </Text>
+);
+
+const styles = StyleSheet.create({
+  textHeading: {
+    fontFamily: 'KaushanScript-Regular',
+    fontSize: 52,
+    fontWeight: 'bold',
+    color: 'white'
+  }
+});
+
+export default MainHeadingText;
