8ac268f6e93f0d0821af140527844adee67a0ee1
Oskari Peltonen <oskari.peltonen@outlook.com>
2018-05-19 15:22:05 +0300

    first screen

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -2,17 +2,35 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
+  ImageBackground,
   StyleSheet
 } from 'react-native';
+
+import backgroundImage from '../../assets/images/background.jpg';
+import MainHeadingtext from '../../components/UI/MainHeadingText/MainHeadingText';
 
 class AuthScreen extends Component {
   render() {
     return (
-      <View>
-        <Text>Login</Text>
-      </View>
+      <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
+        <View style={styles.container}>
+          <MainHeadingtext>Thoughts</MainHeadingtext>
+        </View>
+      </ImageBackground>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    width: '100%'
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 export default AuthScreen;
