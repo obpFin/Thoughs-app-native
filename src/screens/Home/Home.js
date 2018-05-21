478c96f87bae10c6831a6dfb6fe85d5127cbdd0d
Oskari Peltonen <oskari.peltonen@outlook.com>
2018-05-21 17:23:44 +0300

    home screen skeleton

---
+++ src/screens/Home/Home.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  ImageBackground,
+  StyleSheet,
+} from 'react-native';
+
+import backgroundImage from '../../assets/images/background.jpg';
+
+class HomeScreen extends Component {
+
+  render() {
+    return (
+      <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
+        <View style={styles.container}>
+          <Text style={styles.text}>Home</Text>
+        </View>
+      </ImageBackground>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    width: '100%'
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+
+export default HomeScreen;
