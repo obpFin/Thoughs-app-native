6d63b10c2ce1d4f8f48c92dd6a81c6d86e123059
Oskari Peltonen <oskari.peltonen@outlook.com>
2018-05-28 21:47:04 +0300

    added react-native-linear-gradient

--- android/app/src/main/java/com/thoughtsappnative/MainApplication.java
+++ android/app/src/main/java/com/thoughtsappnative/MainApplication.java
@@ -3,6 +3,7 @@ package com.thoughtsappnative;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.reactnativenavigation.NavigationReactPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
             new SplashScreenReactPackage(),
             new VectorIconsPackage(),
             new NavigationReactPackage()
